72dbb699a04ca0a68087df09c59094c2f6c80776
KIWOLY <ikiwoly@gmail.com>
2025-11-05 18:24:05 +0300

    elevated button

--- basic/lib/pages/home.dart
+++ basic/lib/pages/home.dart
@@ -8,21 +8,32 @@ class Homepage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('container', style: TextStyle(fontSize: 30)),
-        centerTitle:true,
+        centerTitle: true,
       ),
-      body: Container(
-        height: 300,
-        width: 300,
-        padding: EdgeInsets.all(20),
-        margin: EdgeInsets.all(30),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.blue,
-        ),
-        child: const Text(
-          'Hello, World!',
-          style: TextStyle(fontSize: 25, color: Colors.white70),
-        ),
+      body: Column(
+        
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 300,
+            width: 200,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.blue,
+            ),
+            child: const Text(
+              'Hello, World!',
+              style: TextStyle(fontSize: 25, color: Colors.white70),
+            ),
+          ),
+          SizedBox(height: 30),
+
+          ElevatedButton(
+            onPressed: (){},
+             child: Text('Go to column page')),
+        ],
       ),
     );
   }
