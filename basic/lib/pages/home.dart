ff3d4a5b5eb9389e9d883b6bfc59122e80d39485
KIWOLY <ikiwoly@gmail.com>
2025-11-05 17:58:45 +0300

    container

--- basic/lib/pages/home.dart
+++ basic/lib/pages/home.dart
@@ -6,11 +6,23 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
-      body: Center(
-        child: Container(
+      appBar: AppBar(
+        title: const Text('container', style: TextStyle(fontSize: 30)),
+        centerTitle:true,
+      ),
+      body: Container(
+        height: 300,
+        width: 300,
+        padding: EdgeInsets.all(20),
+        margin: EdgeInsets.all(30),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
           color: Colors.blue,
-          child: const Text('Hello, World!',style: TextStyle(fontSize: 35, color: Colors.white70)))
+        ),
+        child: const Text(
+          'Hello, World!',
+          style: TextStyle(fontSize: 25, color: Colors.white70),
+        ),
       ),
     );
   }
