5d82a4a2311def9efd59b5d395ef4217fd43ccb8
KIWOLY <ikiwoly@gmail.com>
2025-12-03 10:26:16 +0300

    impliment bottom navifation

--- real/lib/pages/home.dart
+++ real/lib/pages/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:real/pages/home_page.dart';
+import 'package:real/pages/profile.dart';
+
+List<Widget> pages = [Profile(), Home()];
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,9 +11,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HOME", style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),),
+        title: Text(
+          "HOME",
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
-        backgroundColor:Colors.blue,
+        backgroundColor: Colors.blue,
+      ),
+      body: pages.elementAt(0),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+        ],
       ),
     );
   }
